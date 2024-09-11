a17ad0251923e65c1109973813ddce1cf058e8e3
Bence <bencusix@live.it>
2024-09-11 21:44:36 +0200

    tags fixed

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -33,6 +33,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
+</head>
 <body>
     <nav class="top-nav">
         <div class="top-nav-title">
